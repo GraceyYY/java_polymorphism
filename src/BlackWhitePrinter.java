fa0b2eb6d0966427c709f5edda392ad3d2aaa2a6
Grace <lunay0325@gmail.com>
2019-02-18 17:13:43 +0800

    Implement the BlackWhitePrinter class and override the print method

---
+++ src/BlackWhitePrinter.java
@@ -0,0 +1,6 @@
+public class BlackWhitePrinter extends Printer {
+    @Override
+    public void print() {
+        System.out.println("It can only print files in black and white");
+    }
+}
---
+++ src/Printer.java
@@ -0,0 +1,6 @@
+public class Printer {
+    public void print() {
+        System.out.println("It can print files");
+    }
+}
+
